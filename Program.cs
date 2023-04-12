608dd3fe3376c0f885fb85b8c04a4c4bb537b2bb
nosuferatu <kuznetsovpf@gmail.com>
2023-04-13 00:27:45 +0300

    add task2 solution

--- Program.cs
+++ Program.cs
@@ -6,24 +6,62 @@
 // a = -9 b = -3 -> max = -3
 
 
+// Console.Clear();
+
+// Console.WriteLine("Введите число A: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+
+// if (numberA > numberB)
+// {
+//     Console.WriteLine($"max = {numberA}");
+//     Console.WriteLine($"min = {numberB}");
+// }
+// else if (numberA == numberB)
+// {
+//     Console.WriteLine("Число A равно числу B");
+// }
+// else
+// {
+//     Console.WriteLine($"max = {numberB}");
+//     Console.WriteLine($"min = {numberA}");
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Например:
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+
 Console.Clear();
 
 Console.WriteLine("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число С: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
 
 if (numberA > numberB)
 {
-    Console.WriteLine($"max = {numberA}");
-    Console.WriteLine($"min = {numberB}");
+    if (numberA > numberC)
+    {
+        Console.WriteLine($"max = {numberA}");
+    }
+    else
+    {
+        Console.WriteLine($"max = {numberC}");
+    }
 }
-else if (numberA == numberB)
+else if (numberB > numberC)
 {
-    Console.WriteLine("Число A равно числу B");
+    Console.WriteLine($"max = {numberB}");
 }
 else
 {
-    Console.WriteLine($"max = {numberB}");
-    Console.WriteLine($"min = {numberA}");
+    Console.WriteLine($"max = {numberC}");
 }
