9aa797875ddf7834041559e97646a35fd8d66087
nosuferatu <kuznetsovpf@gmail.com>
2023-04-13 00:01:06 +0300

    add task1 solution

--- Program.cs
+++ Program.cs
@@ -5,3 +5,25 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+
+Console.Clear();
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"max = {numberA}");
+    Console.WriteLine($"min = {numberB}");
+}
+else if (numberA == numberB)
+{
+    Console.WriteLine("Число A равно числу B");
+}
+else
+{
+    Console.WriteLine($"max = {numberB}");
+    Console.WriteLine($"min = {numberA}");
+}
